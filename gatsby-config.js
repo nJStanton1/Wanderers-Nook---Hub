55cfa35ca0bde0583b2fa972797fddfdfffbd027
Nathaniel <nstanton1@sheffield.ac.uk>
2025-12-28 22:36:42 +0000

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `Wanderers Nook Hub`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: ["gatsby-plugin-postcss"]
+};
